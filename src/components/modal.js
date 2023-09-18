2db4f5d3a4edf139e18aed8916a9b83fd6ca4187
hennadiirudov <hennadiirudov@gmail.com>
2023-09-18 21:02:38 +0300

    backend features added

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios';
 import '../styles/modal-styles.scss';
 
 function Modal ({modalActive, setModalActive}){
@@ -6,6 +7,8 @@ function Modal ({modalActive, setModalActive}){
    const [userName, setUserName] = useState('');
    const [userEmail, setUserEmail] = useState('');
    const [userBirthDate, setUserBirthDate] = useState('');
+   const [isEmailValid, setValedEmail] = useState('');
+   let data;
 
    const handleNameChange = (event) =>{
       setUserName(event.target.value)
@@ -19,21 +22,35 @@ function Modal ({modalActive, setModalActive}){
       setUserBirthDate(event.target.value)
    }
 
+   const postData = async () => {
+      let emailData = await axios.post('http://localhost:4000/send-email', data)
+      const showdata = await emailData.data;
+      setValedEmail(showdata)
+      console.log(isEmailValid)
+   }
+
+
+   
    const handleSubmit = (event) =>{
       event.preventDefault()
       user.name = userName;
       user.birthDate = userBirthDate;
       user.email = userEmail;
       setUser(user);
-      console.log(user);
+      data = JSON.stringify(user);
+      postData()
+      setUserName('');
+      setUserEmail('');
+      setUserBirthDate('');
    }
-
-
+   
    return(
       <div className={modalActive ? 'modal active' : 'modal'} onClick={()=>setModalActive(false)}>
          <div className="modal-grey"></div>
          <div className="modal-menu-container" onClick={(e)=> e.stopPropagation()}>
-            <form className="modal-input-block" onSubmit={handleSubmit}>
+            <form className="modal-input-block" 
+                  onSubmit={handleSubmit}
+            >
                <div className="modal-title">
                   Подпишись на рассылку,
                   чтобы получить скидку 20% на первый заказ
